fbe58557d2791c897263b8c9ef7006d55a0720aa
Dana Flint <Dana_Flint@msn.com>
2017-01-18 21:28:54 -0600

    took off <> inside keys

--- keys.js
+++ keys.js
@@ -2,8 +2,8 @@ console.log('this is loaded');
 //// Using the require keyword will allow access of all of the exports
 // in the keys.js file
 exports.twitterKeys = {
-    consumer_key: '<HHr15SOaZXA6MToCBXvpSzKoJ>',
-    consumer_secret: '<RXaiMaAijZirp7OOTZ9cw1O5fbyHrtz59jjCZjTxV4lLIR7FrD>',
-    access_token_key: '<74007728-lqpMBXrb7wSuSqN1lv6FPIn8j3vJLd3VjkZm0bVIh>',
-    access_token_secret: '<2z0VsrlIskHnibUO5Q7vAvWhTpWIcRDc7KoSxg4j6dkYw>',
+    consumer_key: 'HHr15SOaZXA6MToCBXvpSzKoJ',
+    consumer_secret: 'RXaiMaAijZirp7OOTZ9cw1O5fbyHrtz59jjCZjTxV4lLIR7FrD',
+    access_token_key: '74007728-lqpMBXrb7wSuSqN1lv6FPIn8j3vJLd3VjkZm0bVIh',
+    access_token_secret: '2z0VsrlIskHnibUO5Q7vAvWhTpWIcRDc7KoSxg4j6dkYw',
 };
